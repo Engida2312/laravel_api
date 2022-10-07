79b469cd8b5370ded882b5f5d4f1c916d6d220bb
jhon <ethiofanjo@gmail.com>
2022-10-06 20:54:17 -0400

    category api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/category', [CategoryController::class, 'getCategory']);
+Route::post('/add-category', [CategoryController::class, 'store']);
+Route::get('/single-category/{id}', [CategoryController::class, 'singleCategory']);
+Route::put('/update-category/{id}', [CategoryController::class, 'update']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/register', [UserController::class, 'store']);
+
