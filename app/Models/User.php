f1417d03f8542c2e239b951b6ecbde29e4659352
NahomAk <nahom.saketa108791@marwadiuniversity.ac.in>
2022-10-13 09:37:20 +0530

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table= 'users';
     protected $fillable = [
-        'name',
-        'email',
+        'firstname',
+        'lastname',
         'password',
+        'email',
+        'github',
+        'linkedin'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\AvatarController;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +14,20 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+|\
 */
+
+Route::get('/editprofile/{id}', [UserController::class, 'edit']);
+Route::put('/updateprofile/{id}', [UserController::class, 'update']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [UserController::class, 'store']);
+// Route::post('/register', [UserController::class, 'store']);
+
+
+/////avatar route
+Route::get('/upload/{id}', [AvatarController::class, 'view']);
+Route::post('/upload/{id}', [AvatarController::class, 'store']);
+Route::put('/uploadImage', [AvatarController::class, 'uploadeImage']);
