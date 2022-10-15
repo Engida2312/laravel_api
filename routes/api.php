69c5d46c8de4e3a028eada9b81f5bfa5095cfb7a
jhon <ethiofanjo@gmail.com>
2022-10-15 09:28:57 -0400

    category and component api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ComponentController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
@@ -15,10 +16,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//component route
+Route::post('/add-component', [ComponentController::class, 'add']);
+Route::get('/component', [ComponentController::class, 'getComponent']);
+Route::get('/single-component/{id}', [ComponentController::class, 'singleComponent']);
+Route::put('/update-component/{id}', [ComponentController::class, 'updateComponent']);
+Route::delete('/delete-component/{id}', [ComponentController::class, 'deleteComponent']);
+//category route
 Route::get('/category', [CategoryController::class, 'getCategory']);
 Route::post('/add-category', [CategoryController::class, 'store']);
 Route::get('/single-category/{id}', [CategoryController::class, 'singleCategory']);
 Route::put('/update-category/{id}', [CategoryController::class, 'update']);
+Route::delete('/delete-category/{id}', [CategoryController::class, 'delete']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
